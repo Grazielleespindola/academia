dfc2a09e19080591348f2b96684debbdcc098376
Grazielleespindola <122610017+Grazielleespindola@users.noreply.github.com>
2023-02-01 13:13:14 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Função para mostrar uma caixa de diálogo na página
+//alert('Olá!');
+
+//var, let, const são tipos de variáveis;
+
+// = significa 'recebe'
+var btnMenu = document.querySelector('.btn-menu');
+var menu = document.querySelector('.menu');
+
+
+// console.log(btnMenu);        Só para verificar se está chamando o que eu quero
+
+
+btnMenu.addEventListener('click', function () {        //Associar o evento de clique ao btnMenu
+  //  console.log('clicou ');
+  //console.log(menu);  
+  //menu.classlist.add('menu-open');
+  //menu.classlist.remove('menu-open);
+
+  menu.classList.toggle('menu-open');       //add e remove a classe
+  btnMenu.classList.toggle('x');
+})
+
